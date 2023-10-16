0b6c85d6a88f5338f426c365b26975a9832819a1
Morai <zulifqarmorai@gmail.com>
2023-10-16 21:40:01 +0500

    copy_of_twogood.comt

---
+++ extra/script.js
@@ -0,0 +1,6 @@
+const cursor = document.getElementById('cursor');
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+});
---
+++ js/script.js
@@ -0,0 +1,82 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function videoconAnimation(){
+    var videocon = document.querySelector("#video-container");   
+var playbtn = document.querySelector("#play");
+videocon.addEventListener("mouseenter", function(){
+    gsap.to(playbtn,{
+        scale: 1,
+        opacity: 1,
+    });
+});
+videocon.addEventListener("mouseleave", function(){
+    gsap.to(playbtn,{
+        scale: 0,
+        opacity: 0,
+    });
+});
+videocon.addEventListener("mousemove", function(dets){
+    gsap.to(playbtn,{ 
+        left:dets.x-80,
+        top: dets.y-68,
+    });
+});
+
+}
+videoconAnimation()
+
+
+
+
+  
+
+function loadingAnimation(){
+    gsap.from("#page1 h1", {
+        y:100,
+        opacity:0,
+        delay:0.5,
+        duration:0.9,
+        stagger:0.3,
+    });
+    gsap.from("#page1 #video-container", {
+        scale:0.9,
+        opacity:0,
+        delay:1.3,
+        duration:0.5,
+    });
+}
+loadingAnimation()
+
+
+function imagebackAnimation(){
+
+document.addEventListener("mousemove", function(dets){
+    gsap.to("#cursor", {
+        left:dets.x,
+        top:dets.y
+    });
+});
+
+// document.querySelector("#child1").addEventListener("mouseenter", function(){
+//     gsap.to("#cursor", {
+//     transform: 'translate(-50%, -50%) scale(1)'
+//     })
+// })
+
+document.querySelectorAll(".child").forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to("#cursor", {
+        transform: 'translate(-50%, -50%) scale(1)'
+        });
+    });
+    elem.addEventListener("mouseleave",function(){
+        gsap.to("#cursor", {
+        transform: 'translate(-50%, -50%) scale(0)'
+        });
+    });
+});
+}
+imagebackAnimation()
